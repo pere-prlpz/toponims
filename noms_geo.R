f1b48974d858dbc4c8c2028ae71eaa19866475db
pere-prlpz <prlpzb@gmail.com>
2020-08-21 15:31:53 +0200

    castells i esglésies

--- noms_geo.R
+++ noms_geo.R
@@ -132,7 +132,7 @@ quick <- function(fila) {
 # filtrem quins elements volem
 #crear <- units[units$dist>.2,] #tot
 #crear <- units[units$dist>.075 & units$Concepte=="hidr." & units$instLabel=="font",]
-crear <- units[units$dist>.1 & grepl("edif|equip",units$Concepte) & grepl("edif|masia",units$instLabel),] #FALTEN ESGLÉSIES, etc.
+crear <- units[units$dist>.075 & grepl("edif|equip",units$Concepte) & grepl("edif|masia|casa|granja|església|castell",units$instLabel),] #FALTEN ESGLÉSIES, etc.
 
 crear$quitem <- gsub("http://www.wikidata.org/entity/", "", crear$item)
 crear <- crear[!duplicated(crear$quitem),]  #trec duplicats per tenir dos municipis o dos P31 a Wikidata
